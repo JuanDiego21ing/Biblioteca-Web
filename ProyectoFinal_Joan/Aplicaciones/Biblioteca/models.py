61557dff71d83f35e4d01340a8506439dd2ff8d7
JoanalvaradoT <144882591+JoanalvaradoT@users.noreply.github.com>
2023-12-02 18:15:10 -0700

    Add files via upload

---
+++ ProyectoFinal_Joan/Aplicaciones/Biblioteca/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class libros(models.Model):
+    id = models.CharField(primary_key=True, max_length=6)
+    titulo = models.CharField(max_length=200)
+  
+
+    def __str__(self):
+        texto = "{0} ({1}) ({2}) ({3}) ({4}) ({5})"
+        return texto.format(self.id,self.titulo,self.autor,self.editorial,self.fecha,self.genero)
+
