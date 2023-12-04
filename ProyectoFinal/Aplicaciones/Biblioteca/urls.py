a916689e978dbd8e1132e8f990bb1fd9b18efa53
DiegoVegaProgra <90019265+JuanDiego21ing@users.noreply.github.com>
2023-12-03 17:48:19 -0700

    Add files via upload
    tercer commit

--- ProyectoFinal/Aplicaciones/Biblioteca/urls.py
+++ ProyectoFinal/Aplicaciones/Biblioteca/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.home)
+    path('', views.home),
+    path('registrarLibros/', views.registrarLibros),
+    path('edicionLibro/<id>', views.edicionLibro),
+    path('editarLibro/', views.editarLibro),
+    path('eliminacionLibro/<id>', views.eliminacionLibro)
 
 ]
