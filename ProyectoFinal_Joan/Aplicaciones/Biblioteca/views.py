2e60bb133fdba19698a7f3a40b5082d6c8eaf86a
DiegoVegaProgra <90019265+JuanDiego21ing@users.noreply.github.com>
2023-12-05 19:57:01 -0700

    Delete ProyectoFinal_Joan directory

--- ProyectoFinal_Joan/Aplicaciones/Biblioteca/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from . models import libros
-
-# Create your views here.
-
-def home(request):
-    librosListas = libros.objects.all()
-    return render(request, "gestionLibros.html", {"libros":librosListas})
